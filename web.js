fc912bbb8a00fd718715349c794c1f2fc9cf4ae0
Peter Nunn <pnunn@intq.com.au>
2013-07-08 23:02:10 +1000

    Changed web.js to use index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
+var fs = require('fs');
+
+var buffer = fs.readFileSync('index.html');
+//console.log(buffer.toString());
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
